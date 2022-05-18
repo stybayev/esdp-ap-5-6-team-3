1cbe16438e744ddb48dae3cffd646618d806ba2e
Сагындыков Рустем <rustem.sagundykov98@gmail.com>
2022-05-18 16:40:26 +0600

    Создал стартовые приложения API для клиента, Джанго приложение для Мерчанта. В settings.py добавил приложения и подключил библиотеку rest_framework

--- e_menu/core/settings.py
+++ e_menu/core/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'api_client',
+    'merchant_app'
 ]
 
 MIDDLEWARE = [
