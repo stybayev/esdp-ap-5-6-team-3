604a726ddd9921b5d7047f57e066ce9b34bbefa3
Bakhytbek <baxa.aseke@gmail.com>
2022-06-12 00:01:58 +0600

    # Убрал лишние данные и изменил ссылку возвращения

--- product/views/product_view.py
+++ product/views/product_view.py
@@ -1,31 +1,15 @@
-from urllib.parse import urlencode
-
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin
 from django.db.models import Q
-from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render, get_object_or_404
 from django.views.generic import CreateView, UpdateView, DetailView, DeleteView, ListView
 from product.helpers import SearchView
 from product.forms import ProductForm, SearchForm
-from django.urls import reverse, reverse_lazy
-from product.models import Product, Review, Category
-from accounts.models import Profile
-from django.db.models import Avg
-from django.db.models import Sum
+from django.urls import reverse
+from product.models import Product, Category
 from googletrans import Translator
-from transliterate import translit, get_translit_function
+from transliterate import get_translit_function
 
 
 translator = Translator()
-
-
-def evaluate(request, pk):
-    product_list = Product.objects.all()
-    product = get_object_or_404(Product, id=request.POST.get('product_id'))
-    evaluations = Review.objects.filter(product=product)
-    product_avg_evaluation = evaluations.aggregate((Avg("evaluation")))
-    print(product_avg_evaluation)
-    return (product_avg_evaluation)
 
 
 class ProductDetailView(DetailView):
@@ -33,36 +17,13 @@ class ProductDetailView(DetailView):
     template_name = 'product/detail_product_view.html'
     model = Product
 
-    # def get_context_data(self, **kwargs):
-    #     kwargs['product'] = get_object_or_404(Product, pk=self.kwargs.get('pk'))
-    #     product = get_object_or_404(Product, pk=self.kwargs.get('pk'))
-    #     evaluations = Review.objects.filter(product=product)
-    #     product_avg_evaluation = evaluations.aggregate(Avg('evaluation'))
-    #     context = {
-    #         'product': product,
-    #         'product_avg_evaluation': product_avg_evaluation
-    #     }
-    #     return context
-
 
 class ProductDeleteView(DeleteView):
     model = Product
-    success_url = reverse_lazy('list_product')
 
-
-class MenuProductCategoryListView(SearchView):
-    template_name = 'product/list_client_category_view.html'
-    model = Category
-    ordering = ("id",)
-    context_object_name = 'categories'
-    paginate_by = 5
-    paginate_orphans = 1
-    search_form = SearchForm
-    search_fields = {
-        'category_name': 'icontains',
-        'translit_category_name': 'icontains',
-        'category_name_translation': 'icontains'
-    }
+    def get_success_url(self):
+        category = get_object_or_404(Category, pk=self.get_object().category_id)
+        return reverse('list_category_product', kwargs={'category': category.category_name})
 
 
 class ProductCategoryListView(SearchView):
@@ -70,7 +31,7 @@ class ProductCategoryListView(SearchView):
     model = Product
     ordering = ("created_at",)
     context_object_name = 'products'
-    paginate_by = 5
+    paginate_by = 15
     paginate_orphans = 1
     search_form = SearchForm
     search_fields = {
@@ -79,6 +40,11 @@ class ProductCategoryListView(SearchView):
         'category__category_name': 'icontains',
         'price': 'icontains',
     }
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = get_object_or_404(Category, category_name=self.kwargs.get('category'))
+        return context
 
     def get_queryset(self):
         return self.model.objects.filter(category__category_name__iexact=self.kwargs.get('category'))
@@ -99,7 +65,11 @@ class ProductCategoryListView(SearchView):
 class ProductCreateView(CreateView):
     template_name = 'product/create_product_view.html'
     form_class = ProductForm
-    redirect_url = 'list_product'
+    object = None
+
+    def get_success_url(self):
+        category = get_object_or_404(Category, pk=self.object.category_id)
+        return reverse('list_category_product', kwargs={'category': category.category_name})
 
     def cyrillic_check(self, text):
         lower = set('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')
@@ -109,7 +79,8 @@ class ProductCreateView(CreateView):
         translit_ru = get_translit_function('ru')
         form = self.form_class(data=request.POST, files=self.request.FILES)
         if form.is_valid():
-            product = form.save(commit=False)
+            self.object = form.save(commit=False)
+            product = self.object
             if self.cyrillic_check(product.product_name) == True:
                 product.translit_product_name = translit_ru(product.product_name, reversed=True)
                 product.product_name_translation = translator.translate(product.product_name, src='ru', dest='en').text
@@ -124,7 +95,7 @@ class ProductCreateView(CreateView):
                 if product.description:
                     product.description_translation = translator.translate(product.description, src='en', dest='ru').text
             product.save()
-            return redirect(self.redirect_url)
+            return redirect(self.get_success_url())
         return render(request, self.template_name,
                       context={
                           'form': form
