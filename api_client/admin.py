649e8d51261e60cb3cf118655fcc0c8fa0690eec
Сагындыков Рустем <rustem.sagundykov98@gmail.com>
2022-07-03 16:17:13 +0600

    Написал приемычные тесты на логирование и создание, изменение записи заказа. Обновил список зависимостей

--- api_client/admin.py
+++ api_client/admin.py
@@ -1,17 +1,17 @@
-from django.contrib import admin
-from api_client.models import Client
-
-
-class UserAdmin(admin.ModelAdmin):
-    list_display = (
-        'full_name', 'email', 'email_verified', 'phone_verified',
-        'first_name_cyrillic',
-        'last_name_cyrillic', 'first_name_latin', 'last_name_latin')
-
-    def get_deleted_objects(self, objs, request):
-        deleted_objects, model_count, perms_needed, protected = \
-            super().get_deleted_objects(objs, request)
-        return deleted_objects, model_count, set(), protected
-
-
-admin.site.register(Client, UserAdmin)
+# from django.contrib import admin
+# from api_client.models import Client
+#
+#
+# class UserAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'full_name', 'email', 'email_verified', 'phone_verified',
+#         'first_name_cyrillic',
+#         'last_name_cyrillic', 'first_name_latin', 'last_name_latin')
+#
+#     def get_deleted_objects(self, objs, request):
+#         deleted_objects, model_count, perms_needed, protected = \
+#             super().get_deleted_objects(objs, request)
+#         return deleted_objects, model_count, set(), protected
+#
+#
+# admin.site.register(Client, UserAdmin)
